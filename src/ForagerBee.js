9bb6977bde9921d4439212b7041a777f6b760d0d
Calin Lewis <calin.lewis@gmail.com>
2018-06-21 19:29:23 -0700

    Complete ForagerBee
    Completed the following tests:
    Test Passed: should have an `age` property that is set to `10` ‣
    Test Passed: should have a `job` property that is set to `find pollen` ‣
    Test Passed: should have a `color` property inherited from `bee` that is set to `yellow` ‣
    Test Passed: should have a `food` property that is inherited from grub ‣
    Test Passed: should have an `eat` method that is inherited from grub ‣
    Test Passed: should have a `canFly` property that is set `true` ‣
    Test Passed: should have a `treasureChest` property that is set to an empty array `[]` ‣
    Test Passed: should have a `forage` method that allows the bee to add a `treasure` to the `treasureChest`

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Grub{
+  constructor(){
+    super();
+    this.age = 10
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = []; 
+    this.forage = function(treasure){
+      this.treasureChest.push(treasure);
+    }
+  }
 };
